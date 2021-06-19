f66268af1901478c1af7a2bd3fb77d60863fd081
RUJRSJ <zxcpporoo12@gmail.com>
2021-06-20 03:04:57 +0800

    [sql] test prepare

--- node-js/api/database/event.js
+++ node-js/api/database/event.js
@@ -1,5 +1,42 @@
-exports.get_event_by_time = function (req, res) {
-    var start_year, start_month, start_day;
-    var end_year, end_month, end_day;
-    var sql = "SELECT * FROM Event natural join time_slot WHERE start_year BETWEEN " + start_year + " AND " + end_year + "";
+var mysql = require('mysql');
+
+var fs = require('fs')
+var ini = require('ini')
+
+var config = ini.parse(fs.readFileSync('./conf.ini', 'utf-8'));
+
+var conn = mysql.createConnection({
+    host: config.server.host,
+    user: config.server.user,
+    password: config.server.password,
+    port: config.server.port,
+    //database: 'team11',
+});
+
+exports.get_event_by_time = function (req, res, start_date, end_date) {
+    var sql = "SELECT * FROM Event WHERE time_start_date BETWEEN " + start_date +
+        " AND " + end_date + " AND time_end_date BETWEEN " + start_date + " AND " + end_date;
+    conn.connect(function (err) {
+        //if (err) throw err;
+    });
+    conn.query(sql, function (err, rows, fields) {
+        //if (err) throw err;
+        console.log(rows);
+    });
 }
+
+var start_date = new Date(2020, 6, 19);
+var end_date = new Date(2021, 6, 19);
+start_date = start_date.toISOString().slice(0, 10);
+end_date = end_date.toISOString().slice(0, 10);
+var sql = "SELECT * FROM Event WHERE time_start_date BETWEEN " + start_date +
+    " AND " + end_date + " AND time_end_date BETWEEN " + start_date + " AND " + end_date;
+conn.connect(function (err) {
+    //if (err) throw err;
+});
+conn.query(sql, function (err, rows, fields) {
+    //if (err) throw err;
+    console.log("do: " + sql);
+    console.log(rows);
+});
+conn.end();
--- node-js/server.js
+++ node-js/server.js
@@ -1,12 +1,13 @@
 var express = require('express')
     , app = express();
 var login = require('./api/model/login');
+var event = require('./api/model/event');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', login);
-
+app.use('/api', event);
 
 var sever = app.listen(3000, function () {
     console.log('listening on port 3000');
