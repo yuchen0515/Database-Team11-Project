6bb772e4272bb484e37196e2e5ffd9640a210c29
yuchen0515 <i98565412@gmail.com>
2021-06-19 23:37:52 +0800

    [sql] ddl.sql remove reference for time_slot

--- DDL.sql
+++ DDL.sql
@@ -11,9 +11,7 @@ create table Stuff
 (
     stuff_ID        char(8),
     account_ID      char(8),
-    /*
-    record_date     varchar(4),
-    */
+
     record_date_start_date DATE,
     record_date_start_time TIME,
     record_date_end_date DATE,
@@ -23,7 +21,6 @@ create table Stuff
         check (status in ('Y', 'N', 'A')), 
         /* Y: Done, N: Not yet, A: Abandon */
     primary key (stuff_ID),
-    foreign key (record_date) references time_slot(time_slot_id) on delete set null,
     foreign key (account_ID) references UserData(account_ID) on delete cascade
 ) ENGINE=INNODB;
 
@@ -32,17 +29,12 @@ create table Event
 (
     event_ID        char(8),
     account_ID      char(8),
-    /*
-    time            varchar(4),
-    */
+
     time_start_date DATE,
     time_start_time TIME,
     time_end_date   DATE,
     time_end_time   TIME,
 
-    /*
-    buf_time        varchar(50),
-    */
     buf_time        TIME,
     title           varchar(50),
     content         varchar(500),
@@ -50,7 +42,6 @@ create table Event
         check (status in ('Y', 'N', 'A')), 
         /* Y: Done, N: Not yet, A: Abandon */
     primary key (event_ID),
-    foreign key (time) references time_slot(time_slot_id) on delete set null,
     foreign key (account_ID) references UserData(account_ID) on delete cascade
 ) ENGINE=INNODB;
 
@@ -58,9 +49,7 @@ create table Project
 (
     project_ID      char(8),
     account_ID      char(8),
-    /*
-    deadline        varchar(4),
-    */
+
     deadline_start_date DATE,
     deadline_start_time TIME,
     deadline_end_date DATE,
@@ -73,7 +62,6 @@ create table Project
         check (status in ('Y', 'N', 'A')), 
         /* Y: Done, N: Not yet, A: Abandon */
     primary key (project_ID),
-    foreign key (deadline) references time_slot(time_slot_id) on delete set null,
     foreign key (account_ID) references UserData(account_ID) on delete cascade
 ) ENGINE=INNODB;
 
@@ -82,9 +70,7 @@ create table Task
     task_ID         char(8),
     project_ID      char(8),
     destination     varchar(200),
-    /*
-    time_req        varchar(4),
-    */
+
     time_req_start_date DATE,
     time_req_start_time TIME,
     time_req_end_date DATE,
@@ -96,23 +82,4 @@ create table Task
     highlight       char(1),
     primary key (task_ID),
     foreign key (project_ID) references Project(project_ID) on delete cascade,
-    foreign key (time_req) references time_slot(time_slot_id) on delete set null
 )  ENGINE=INNODB;
-
-/*
-create table time_slot
-(
-    time_slot_id    varchar(4),
-    start_year      numeric(4) check (start_year > 1970 and start_year < 2050),
-    start_month     numeric(2) check (start_month >= 1 and start_month <= 12),
-    start_day	    numeric(2) check (start_day >= 1 and start_day <= 31),
-    start_hr		numeric(2) check (start_hr >= 0 and start_hr < 24),
-    start_min		numeric(2) check (start_min >= 0 and start_min < 60),
-    end_year        numeric(4) check (end_year > 1970 and end_year < 2050),
-    end_month       numeric(2) check (end_month >= 1 and end_month <= 12),
-    end_day			numeric(2) check (end_day >= 1 and end_day <= 31),
-    end_hr			numeric(2) check (end_hr >= 0 and end_hr < 24),
-    end_min		    numeric(2) check (end_min >= 0 and end_min < 60),
-    primary key (time_slot_id, start_year, start_month, start_day, start_hr, start_min)
-)  ENGINE=INNODB;
-*/
