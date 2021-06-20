9627bfef3febf4dc798c2fafaa93838ad7472d28
RuiRabbit <rabbit922w@gmail.com>
2021-06-20 18:58:11 +0800

    Finish memo page

--- vue-js-client-crud/src/views/Memo.vue
+++ vue-js-client-crud/src/views/Memo.vue
@@ -82,6 +82,7 @@
                                         text
                                         v-bind="attrs"
                                         v-on="on"
+                                        block
                                         @click="loadProjectData(item)"
                                         >
                                         To Project
@@ -286,6 +287,7 @@
                                         text
                                         v-bind="attrs"
                                         v-on="on"
+                                        block
                                         @click="loadEventData(item)"
                                         >
                                         To Calendar
@@ -500,6 +502,54 @@
                                     <!-- <v-btn text @click="console.log(item.title)">{{choice.title}}</v-btn> -->
                                     <!-- <v-list-item-title>{{choice.choice}}</v-list-item-title> -->
                                 </v-list-item>
+                                <v-list-item
+                                    :key="deleteStuff"
+                                >
+                                    <v-dialog
+                                        v-model="dialog_delete"
+                                        persistent
+                                        max-width="600px"
+                                    >
+                                    
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-btn
+                                            text
+                                            v-bind="attrs"
+                                            v-on="on"
+                                            color="error"
+                                            block
+                                            @click="loadDeleteId"
+                                            >
+                                            Delete
+                                            </v-btn>
+                                        </template>
+
+                                        <v-card>
+                                            <v-card-title class="text-h5">
+                                            Warning
+                                            </v-card-title>
+                                            <v-card-text>Are you sure you want to delete this memo?</v-card-text>
+                                            <v-card-actions>
+                                                <v-spacer></v-spacer>
+                                                <v-btn
+                                                    color="blue darken-1"
+                                                    text
+                                                    @click="dialog_delete = false"
+                                                >
+                                                    Cancel
+                                                </v-btn>
+                                                <v-btn
+                                                    color="error"
+                                                    text
+                                                    @click="dialog_delete = false"
+                                                >
+                                                    Delete
+                                                </v-btn>
+                                            </v-card-actions>
+                                        </v-card>
+
+                                    </v-dialog>
+                                </v-list-item>
                             </v-list>
                         </v-menu>
                     </v-col>
@@ -515,6 +565,7 @@ export default {
         return{
             dialog_project: false,
             dialog_calendar: false,
+            dialog_delete: false,
             importance: [
                 'Low',
                 'Medium',
@@ -543,6 +594,7 @@ export default {
                 endDate: new Date().toISOString().substr(0, 10),
                 endTime: new Date().toISOString().substr(11, 5),
             },
+            delete_id: "",
             memo_items: [
                 {
                     id: "1",
@@ -552,11 +604,16 @@ export default {
                 {
                     id: "2",
                     title: "title2",
-                    content: "content"
+                    content: "contentasdfjasoidfoasjfdiasodjfijasodifijadsjoaifoaidsjfoiasjdiofjoias"
                 },
                 {
                     id: "3",
                     title: "title3",
+                    content: "content"
+                },
+                {
+                    id: "4",
+                    title: "title4",
                     content: "content"
                 },
             ]
@@ -590,6 +647,9 @@ export default {
             this.event_data.endDate = new Date(+(new Date() - new Date().getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
             this.event_data.endTime = new Date(+(new Date() - new Date().getTimezoneOffset() * 60000)).toISOString().substr(11, 5);
             console.log(new Date().getTimezoneOffset())
+        },
+        loadDeleteId (item) {
+            this.delete_id = item.id;
         },
         
         validate () {
