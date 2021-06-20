963fc2c24b5340aac54d23d716c5d0bb93914e0e
RUJRSJ <zxcpporoo12@gmail.com>
2021-06-20 19:17:22 +0800

    [login] status in json

--- node-js/api/database/login.js
+++ node-js/api/database/login.js
@@ -4,25 +4,25 @@ exports.login = function (req, res) {
 
     var username = req.query.username;
     var password = req.query.password;
-
-    console.log("get username: ", username);
-    console.log("get password: ", password);
-
+    /*
+        console.log("get username: " + username);
+        console.log("get password: " + password);
+    */
     var sql = "SELECT * FROM UserData WHERE account_ID = '" + username + "'";
 
     conn.query(sql, function (err, rows, fields) {
         if (!rows.length) {
-            res.status(404).json(null);
+            res.status(404).json({ status: 404 });
             console.log("err 404");
         }
         else if (rows[0].account_ID == username && rows[0].password == password) {
             //req.session.account_ID = username;
             //req.session.password = password;
-            res.status(200).json({ username: username });
+            res.status(200).json({ username: username, status: 200 });
             console.log("correct 200");
         }
         else if (rows[0].account_ID == username && rows[0].password != password) {
-            res.status(406).json(null);
+            res.status(406).json({ status: 406 });
             console.log("err 406");
         }
     });
